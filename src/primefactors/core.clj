983b51744332a1233367f4911b9e8b5b609fc25f
adammartin <ebondark.od@gmail.com>
2012-03-03 23:17:28 -0600

    solve for 4 using recursion

--- src/primefactors/core.clj
+++ src/primefactors/core.clj
@@ -1,7 +1,8 @@
 (ns primefactors.core)
 
 (defn primefactors [n]
-	(if (< 1 n)
-		(list n)
-		(list))
+	(cond (<= n 1) (list)
+		  (= 0 (rem n 2)) (conj (primefactors (/ n 2)) 2)
+		  :else (list n)
+	)
 )
