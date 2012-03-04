9a928a2b28bc958eb4c5b40f94952441532903c4
adammartin <ebondark.od@gmail.com>
2012-03-04 15:39:06 -0600

    using the accumulator

--- src/primefactors/core.clj
+++ src/primefactors/core.clj
@@ -3,7 +3,7 @@
 (defn primefactors 
 	([n] (primefactors n 2 '()))
 	([n candidate accumulator] 
-		(cond	(<= n 1) (list)
+		(cond	(<= n 1) (reverse accumulator)
 		  		(= 0 (rem n candidate)) (conj (primefactors (/ n candidate)) candidate)
 		  		:else (primefactors n (inc candidate) accumulator)
 		)
