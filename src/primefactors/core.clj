971945372d21a65f8ce493fab6da778d79d8d60d
adammartin <ebondark.od@gmail.com>
2012-03-04 15:38:06 -0600

    adding testing for larger primefactor tests to protect against blowing the stack

--- src/primefactors/core.clj
+++ src/primefactors/core.clj
@@ -1,11 +1,11 @@
 (ns primefactors.core)
 
 (defn primefactors 
-	([n] (primefactors n 2))
-	([n candidate] 
+	([n] (primefactors n 2 '()))
+	([n candidate accumulator] 
 		(cond	(<= n 1) (list)
 		  		(= 0 (rem n candidate)) (conj (primefactors (/ n candidate)) candidate)
-		  		:else (primefactors n (inc candidate))
+		  		:else (primefactors n (inc candidate) accumulator)
 		)
 	)
 )
--- test/primefactors/test/core.clj
+++ test/primefactors/test/core.clj
@@ -16,3 +16,7 @@
 (fact (primefactors 8) => (list 2 2 2))
 
 (fact (primefactors 9) => (list 3 3))
+
+(fact (primefactors 1958736) => (list 2 2 2 2 3 13 43 73))
+
+(future-fact (primefactors 1000001) => (list 101 9901))
