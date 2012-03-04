9deb539cc78004ff2dfe82f2cdf59b3740cae62d
adammartin <ebondark.od@gmail.com>
2012-03-03 23:34:08 -0600

    forgot to fully use candidate

--- src/primefactors/core.clj
+++ src/primefactors/core.clj
@@ -4,7 +4,7 @@
 	([n] (primefactors n 2))
 	([n candidate] 
 		(cond	(<= n 1) (list)
-		  		(= 0 (rem n candidate)) (conj (primefactors (/ n candidate)) 2)
+		  		(= 0 (rem n candidate)) (conj (primefactors (/ n candidate)) candidate)
 		  		:else (list n)
 		)
 	)
